3ceb5956d914099ede53527205b214ffd6b3abf2
mykolaSribnii <mykolaSribnii@users.noreply.github.com>
2024-04-19 09:49:42 +0000

    deploy: 374b18fd1f33c134aa1067e1b03d10cfa5d07b6a

--- src/pages/pricing-page.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from '@playwright/test';
-import { BasePage } from './base-page';
-
-export class PricingPage extends BasePage {
-    public constructor(page: Page) {
-        super(page);
-    }
-}
