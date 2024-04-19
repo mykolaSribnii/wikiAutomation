03662950b42afd10e7f26eeb4211e90530a41e69
mykolaSribnii <mykolaSribnii@users.noreply.github.com>
2024-04-19 09:37:50 +0000

    deploy: 203dd6808d16ef5ff30bc851112cab1809796f6c

--- 20240419_090923Z/src/pages/base-page.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export abstract class BasePage {
-    public constructor(protected readonly page: Page) {}
-
-    public async navigate() {
-        this.page.goto('https://www.aura.com', { waitUntil: 'load' });
-    }
-}
